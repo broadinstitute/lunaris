25161655b23403e3ddd8674556def92359dca63a
curoli <curoli@gmail.com>
2020-03-24 16:02:14 -0400

    Reading data: more debugging.

--- src/main/scala/lunaris/app/LunarisApp.scala
+++ src/main/scala/lunaris/app/LunarisApp.scala
@@ -23,14 +23,16 @@ object LunarisApp {
     val posCol = 1
     val featureReader =
       AbstractFeatureReader.getFeatureReader(featureResource, RecordCodec(chromCol, posCol), tabixIndex)
-    val header = featureReader.getHeader
-    println(header)
-    val recordIterator = featureReader.query("1", 1, 100000)
-    while (recordIterator.hasNext) {
+//    val header = featureReader.getHeader
+//    println(header)
+//    val recordIterator = featureReader.query("1", 1, 100000)
+    val recordIterator = featureReader.iterator()
+    var count: Int = 0
+    while (count < 10 && recordIterator.hasNext) {
       val record = recordIterator.next()
       println(record)
+      count += 1
     }
-
   }
 
 }
--- src/main/scala/lunaris/stream/RecordCodec.scala
+++ src/main/scala/lunaris/stream/RecordCodec.scala
@@ -5,27 +5,42 @@ import htsjdk.tribble.readers.LineIterator
 import lunaris.genomics.{Chromosome, Locus}
 
 case class RecordCodec(chromCol: Int, posCol: Int) extends AsciiFeatureCodec[Record](classOf[Record]){
+
+  def isHeader(line: String): Boolean = line.startsWith("#")
+
   override def canDecode(path: String): Boolean = path.endsWith("tsv.gz")
 
   override def readActualHeader(reader: LineIterator): Header = {
-    val headerLineRaw = reader.next()
-    println(headerLineRaw)
-    var headerLineTrimmed: String = headerLineRaw
-    if(headerLineTrimmed.startsWith("#")) {
-      headerLineTrimmed = headerLineTrimmed.substring(1)
+    reader.hasNext
+    val line = reader.peek()
+    println("header")
+    println(line)
+    val nextLine = reader.next()
+    println("header next line")
+    println(nextLine)
+    Thread.dumpStack()
+    if(line != null && isHeader(line)) {
+      val headerLine = line.substring(1).trim()
+      val colNames = headerLine.split("\t")
+      Header(colNames, chromCol, posCol)
+    } else {
+      null
     }
-    headerLineTrimmed = headerLineTrimmed.trim
-    val colNames = headerLineTrimmed.split("\t")
-    Header(colNames, chromCol, posCol)
   }
 
   override def decode(line: String): Record = {
-    println(line.map(_.toHexString).mkString(" "))
-//    println(line)
-    val values = line.trim.split("\t")
-    val chromosome = Chromosome.parse(values(chromCol)).get
-    val pos = values(posCol).toLong
-    val locus = Locus(chromosome, pos)
-    Record(locus, values)
+//    println(line.map(_.toHexString).mkString(" "))
+    println("decode")
+    println(line)
+    Thread.dumpStack()
+    if(isHeader(line)) {
+      null
+    } else {
+      val values = line.trim.split("\t")
+      val chromosome = Chromosome.parse(values(chromCol)).get
+      val pos = values(posCol).toLong
+      val locus = Locus(chromosome, pos)
+      Record(locus, values)
+    }
   }
 }
