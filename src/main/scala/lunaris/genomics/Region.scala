9ccd68cab9ace6abb60832d9136654d3a7f0c132
curoli <curoli@gmail.com>
2020-05-22 09:00:26 -0400

    Object stream joiner, part 1.

--- src/main/scala/lunaris/genomics/Region.scala
+++ src/main/scala/lunaris/genomics/Region.scala
@@ -3,6 +3,8 @@ package lunaris.genomics
 case class Region(begin: Int, end: Int) extends Ordered[Region] {
   def asString: String = s"$begin-$end"
 
+  override def toString: String = asString
+
   def overlaps(oRegion: Region): Boolean = begin < oRegion.end && end > oRegion.begin
 
   override def compare(that: Region): Int = {
@@ -11,19 +13,6 @@ case class Region(begin: Int, end: Int) extends Ordered[Region] {
       beginComparison
     } else {
       end - that.end
-    }
-  }
-}
-
-object Region {
-  object RegionOrdering extends Ordering[Region] {
-    override def compare(region1: Region, region2: Region): Int = {
-      val beginComparison = region1.begin - region2.begin
-      if(beginComparison != 0) {
-        beginComparison
-      } else {
-        region1.end - region2.end
-      }
     }
   }
 }
--- src/main/scala/lunaris/streams/JoinedObjectsEitherator.scala
+++ src/main/scala/lunaris/streams/JoinedObjectsEitherator.scala
@@ -10,8 +10,30 @@ class JoinedObjectsEitherator(objectEtors: Seq[Eitherator[ObjectValue]],
   val locationOrdering = new LocusOrdering(chromOrdering)
   var snagOpt: Option[Snag] = None
 
-  class EtorWithBuffer(val objectEtor: Eitherator[ObjectValue]) {
+  private val etorsWithBuffers = objectEtors.map(new EtorWithBuffer(_))
 
+  class EtorWithBuffer(val objectEtor: Eitherator[ObjectValue]) {
+    var snagOpt: Option[Snag] = None
+    var underlyingIsExhausted: Boolean = false
+    var objectsHere: Seq[ObjectValue] = Seq.empty
+    var objectBeyondOpt: Option[ObjectValue] = None
+
+    def loadAllHere(): Unit = {
+      while(snagOpt.isEmpty && !underlyingIsExhausted && objectBeyondOpt.isEmpty) {
+        objectEtor.next() match {
+          case Left(snag) => snagOpt = Some(snag)
+          case Right(Some(nextObject)) =>
+            if(objectsHere.isEmpty) {
+              objectsHere = Seq(nextObject)
+            } else if (objectsHere.head.locus == nextObject.locus) {
+              objectsHere :+= nextObject
+            } else {
+              objectBeyondOpt = Some(nextObject)
+            }
+          case Right(None) => underlyingIsExhausted = true
+        }
+      }
+    }
   }
 
 
