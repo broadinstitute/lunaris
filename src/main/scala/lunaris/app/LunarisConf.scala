b16229a3342dd7d64b45e1d44acd5621f88fef9c
curoli <curoli@gmail.com>
2020-06-16 12:24:15 -0400

    Fixing and polishing CLI. Complete move to version 1.1.0.

--- src/main/scala/lunaris/app/LunarisConf.scala
+++ src/main/scala/lunaris/app/LunarisConf.scala
@@ -3,13 +3,22 @@ package lunaris.app
 import org.rogach.scallop.{ScallopConf, Subcommand}
 
 class LunarisConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  version(LunarisInfo.versionLong)
+  banner(
+    """Usage: lunaris batch|server ...
+      |Lunaris is a stream processor to extract, combine and munge genomics-related data from location-sorted block-gzipped tabix-indexed files.
+      |Files can be local, on Google Cloud Storage or on Terra.
+      |""".stripMargin)
+  footer("For more or more updated information, check https://github.com/broadinstitute/lunaris")
   val batch = new Subcommand("batch") {
-    val requestFile = opt[String](required = true)
+    banner("Loads a request from file and executes it.")
+    val requestFile = opt[String](descr = "Location of file containing request in JSON.", required = true)
   }
   addSubcommand(batch)
   val server = new Subcommand("server") {
-
+    banner("Web service - not yet implemented.")
   }
   addSubcommand(server)
+  requireSubcommand()
   verify()
 }
