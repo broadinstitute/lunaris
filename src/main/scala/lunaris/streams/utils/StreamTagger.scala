f16c7282cc710fda9f95d4f58bdf9c875731f061
curoli <curoli@gmail.com>
2020-10-05 15:33:26 +0300

    Fix exception thrown when no pre-computed data in range covered by inputs.

--- src/main/scala/lunaris/streams/utils/StreamTagger.scala
+++ src/main/scala/lunaris/streams/utils/StreamTagger.scala
@@ -7,9 +7,12 @@ object StreamTagger {
   case class TaggedItem[T, S](item: T, sourceId: S, isLast: Boolean)
 
   def tagSource[T, M, S](source: Source[T, M], sourceId: S): Source[TaggedItem[T, S], M] = {
-    source.map(Some(_)).concat(Source.single(None)).sliding(2).map { itemOpts =>
-      val isLast = itemOpts.size < 2 || itemOpts(1).isEmpty
-      TaggedItem[T, S](itemOpts.head.get, sourceId, isLast)
+    source.map(Some(_)).concat(Source.single(None)).sliding(2).mapConcat { itemOpts =>
+      if (itemOpts.size == 2) {
+        Seq(TaggedItem[T, S](itemOpts.head.get, sourceId, isLast = itemOpts(1).isEmpty))
+      } else {
+        Seq()
+      }
     }
   }
 
