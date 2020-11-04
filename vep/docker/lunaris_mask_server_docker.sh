fce6417d60beb8466e14b81c1957c68013904467
curoli <curoli@gmail.com>
2020-11-04 19:27:46 +0100

    Lunaris Variant Mask Server Docker image, part 3; advancing version from 1.5.3 to 1.5.4.

---
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cmd=$@
+image=lunaris-variant-mask-server:1.5.3
+
+echo "This is Lunaris Variant Mask Server Docker run script"
+
+if [[ -z ${cmd} ]]; then
+  cmd="lunaris vep --config-file configs/lunarisDocker.conf"
+  echo "No command specified, that's fine, defaulting to:"
+  echo $cmd
+  echo "This should start the Lunaris Variant Mask Server"
+fi
+
+lunaris_vep_dir=$HOME/lunaris/vep
+aux_dir=$lunaris_vep_dir/aux
+data_dir=$lunaris_vep_dir/data
+
+echo "But first, if not done already, building image ${image}."
+sudo docker build ~/git/lunaris/vep/docker/ -t ${image}
+echo "Done building image ${image}, now running command."
+sudo docker run -p 8080:8080 -v $aux_dir:/mnt/aux -v $data_dir:/mnt/data -it ${image} ${cmd}
+echo "Done with Lunaris Variant Mask Server Docker run script."
