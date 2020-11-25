f3da9651646c5f6b05173a4f9cc562ec3816ab23
curoli <curoli@gmail.com>
2020-11-25 17:23:09 +0100

    Fix VEP+Lunaris Dockerfile: fix vep invocation; advance version from 1.5.14 to 1.5.15.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.5.14"
+  val version: String = "1.5.15"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
