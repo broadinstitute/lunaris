f213aff9c72659186560ee24592419346933c04c
curoli <curoli@gmail.com>
2020-11-25 18:46:19 +0100

    Fix VEP+Lunaris Dockerfile: fix configuration; advance version from 1.5.15 to 1.5.16.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 import sbt.project
 
-val lunarisV = "1.5.15"
+val lunarisV = "1.5.16"
 val scalaV = "2.13.3"
 val yootilzV = "0.1.5"
 val scalaTestV = "3.2.0"
--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.5.15
+image=lunaris-variant-mask-server:1.5.16
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
 if [[ -z ${cmd} ]]; then
-  cmd="lunaris vep --config-file configs/lunarisDocker.conf"
+  cmd="lunaris vep --config-file configs/lunarisVepDocker.conf"
   echo "No command specified, that's fine, defaulting to:"
   echo $cmd
   echo "This should start the Lunaris Variant Mask Server"
@@ -15,9 +15,10 @@ fi
 lunaris_vep_dir=$HOME/lunaris/vep
 aux_dir=$lunaris_vep_dir/aux
 data_dir=$lunaris_vep_dir/data
+work_dir=$lunaris_vep_dir/work
 
 echo "But first, if not done already, building image ${image}."
 sudo docker build ~/git/lunaris/vep/docker/ -t ${image}
 echo "Done building image ${image}, now running command."
-sudo docker run -p 8080:8080 -v $aux_dir:/mnt/aux -v $data_dir:/mnt/data -it ${image} ${cmd}
+sudo docker run -p 8080:8080 -v $aux_dir:/mnt/aux -v $data_dir:/mnt/data -v $work_dir:/mnt/work -it ${image} ${cmd}
 echo "Done with Lunaris Variant Mask Server Docker run script."
