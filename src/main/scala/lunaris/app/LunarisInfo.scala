5415f07d1acd7aa60933b13e8c24d0b518ab7959
curoli <curoli@gmail.com>
2020-12-07 19:21:18 +0100

    Fix missing sample input file; advancing version from 1.5.21 to 1.5.22.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.5.21"
+  val version: String = "1.5.22"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
