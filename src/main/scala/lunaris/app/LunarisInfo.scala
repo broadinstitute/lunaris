6e07655301c56981bace7dc6e4217caa22aacd9e
curoli <curoli@gmail.com>
2020-12-07 19:05:24 +0100

    Fixing masks; adding sample input file download; advancing version from 1.5.20 to 1.5.21.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.5.20"
+  val version: String = "1.5.21"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
