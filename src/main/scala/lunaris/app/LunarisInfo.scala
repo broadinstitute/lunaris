bd3e149b9c37b41f7d9368ed36869fa299092640
curoli <curoli@gmail.com>
2020-12-10 15:11:20 +0100

    More WebUI tweaks: step numbers and better text; advancing version from 1.5.23 to 1.5.24

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.5.23"
+  val version: String = "1.5.24"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.5.23
+image=lunaris-variant-mask-server:1.5.24
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
