b12036587e89a8e394342e8d1243416267602d7d
curoli <curoli@gmail.com>
2020-12-15 20:08:43 +0100

    WebUI tweaks; advancing version from 1.5.28 to 1.6.0.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.5.28"
+  val version: String = "1.6.0"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.5.28
+image=lunaris-variant-mask-server:1.6.0
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
