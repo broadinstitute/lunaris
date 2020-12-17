b98a5602171455a26a22937b123b71e24ac8d1da
curoli <curoli@gmail.com>
2020-12-17 20:25:55 +0100

    Advancing version from 1.6.5 to 1.6.6.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.6.5"
+  val version: String = "1.6.6"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
