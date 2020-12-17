58ad01001a8af14867e6331215d7c5c15d3fb804
curoli <curoli@gmail.com>
2020-12-17 11:47:45 +0100

    Removing 'sudo' again for command in container, instead map container port 8080 to host port 80; advancing version from 1.6.2 to 1.6.3.

--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.6.2
+image=lunaris-variant-mask-server:1.6.3
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
 if [[ -z ${cmd} ]]; then
-  cmd="sudo lunaris vep --config-file configs/lunarisVepDocker.conf"
+  cmd="lunaris vep --config-file configs/lunarisVepDocker.conf"
   echo "No command specified, that's fine, defaulting to:"
   echo $cmd
   echo "This should start the Lunaris Variant Mask Server"
@@ -21,6 +21,6 @@ cache_dir=$HOME/.vep
 echo "But first, if not done already, building image ${image}."
 sudo docker build ~/git/lunaris/vep/docker/ -t ${image}
 echo "Done building image ${image}, now running command."
-sudo docker run -p 80:80 -v $aux_dir:/mnt/aux -v $data_dir:/mnt/data -v $work_dir:/mnt/work \
+sudo docker run -p 80:8080 -v $aux_dir:/mnt/aux -v $data_dir:/mnt/data -v $work_dir:/mnt/work \
   -v $cache_dir:/opt/vep/.vep -it ${image} ${cmd}
 echo "Done with Lunaris Variant Mask Server Docker run script."
