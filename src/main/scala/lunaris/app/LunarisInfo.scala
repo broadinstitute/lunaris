7b12702c58c945b95d2a5967389b897278c3851a
curoli <curoli@gmail.com>
2020-12-17 11:59:14 +0100

    Fix: port in config for Docker mode needs to be 8080; advancing version from 1.6.3 to 1.6.4.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.6.3"
+  val version: String = "1.6.4"
   val versionLong: String = s"Lunaris Version $version (c) 2020 Broad Institute"
 }
--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.6.3
+image=lunaris-variant-mask-server:1.6.4
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
