ede212745e7ebac27b13a89ab11ee93c47c14dab
curoli <curoli@gmail.com>
2021-01-24 22:46:43 +0100

    Integrating DB into EGG Server, part 3.

--- src/main/scala/lunaris/vep/db/EggDb.scala
+++ src/main/scala/lunaris/vep/db/EggDb.scala
@@ -1,11 +1,13 @@
 package lunaris.vep.db
 
 import better.files.File
-import lunaris.vep.VepFileManager.ResultId
-import lunaris.vep.db.EggDb.rightOrThrow
+import lunaris.vep.VepFileManager.{ResultId, ResultStatus}
+import lunaris.vep.db.EggDb.{JobRecord, rightOrThrow}
 import musha.sql.{Sql, SqlType}
 import musha.{Musha, MushaConfig, MushaQuery}
 import org.broadinstitute.yootilz.core.snag.Snag
+import musha.map.FunBuilder._
+import musha.map.FieldExtractors._
 
 class EggDb(mushaConfig: MushaConfig) {
   private val musha = Musha(mushaConfig)
@@ -23,11 +25,52 @@ class EggDb(mushaConfig: MushaConfig) {
 
   createTableIfNotExist()
 
+  private def messagesToString(messages: Seq[String]): String = messages.mkString("\n")
+  private def stringToMessages(string: String): Seq[String] = string.split("\n")
+
   def createTableIfNotExist(): Unit = {
     rightOrThrow(musha.runUpdate(MushaQuery.update(Sql.createTableIfNotExists(jobsTable))))
   }
 
-  //  TODO
+  def newSubmittedJob(inputFile: File, outputFile: File): Either[Snag, JobRecord] = {
+    val id = ResultId.createNew()
+    val time = System.currentTimeMillis()
+    val messages = Seq.empty[String]
+    val submittedStatus = ResultStatus.createSubmitted(time, messages)
+    val statusType = submittedStatus.statusType
+    val message = submittedStatus.message
+    val sql = Sql.insert(
+        jobsTable, idCol.withValue(id.toString), inputFileCol.withValue(inputFile.toString),
+        outputFileCol.withValue(outputFile.toString), statusTypeCol.withValue(statusType.toString),
+        messageCol.withValue(message), messagesCol.withValue(messagesToString(messages))
+      )
+    val query = MushaQuery.update(sql)
+    musha.runUpdate(query).filterOrElse(_ > 0, Snag("Insert of new record failed")).map { _ =>
+      JobRecord(id, inputFile, outputFile, statusType, message, messages, time, time)
+    }
+  }
+
+  def updateJobStatus(id: ResultId, status: ResultStatus): Either[Snag, Unit] = {
+    val mTime = System.currentTimeMillis()
+    val sql = Sql.merge(
+      jobsTable, idCol.withValue(id.string), statusTypeCol.withValue(status.statusType.toString),
+      messageCol.withValue(status.message), messagesCol.withValue(messagesToString(status.snagMessages)),
+      mTimeCol.withValue(mTime)
+    )
+    val query = MushaQuery.update(sql)
+    musha.runUpdate(query).filterOrElse(_ > 0, Snag(s"Update of record $id failed.")).map( _ => ())
+  }
+
+  def getJob(id: ResultId): Either[Snag, JobRecord] = {
+    val sql = Sql.select(jobsTable, Sql.Equals(idCol, id.string))
+    val query = MushaQuery.rowsIter(sql){ rs =>
+      ???  //  TODO field extractors from column definitions
+    }
+    ???
+  }
+
+  def deleteJob(id: ResultId): Either[Snag, Unit] = ???
+
 }
 
 object EggDb {
@@ -50,6 +93,7 @@ object EggDb {
     }
   }
 
-  case class FilesRecord(id: ResultId, inputFile: File, outputFile: File)
+  case class JobRecord(id: ResultId, inputFile: File, outputFile: File, statusType: ResultStatus.Type, message: String,
+                       messages: Seq[String], ctime: Long, mTime: Long)
 
 }
