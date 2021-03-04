9e3a271accfc3129892f7fc87d246f2d26bf490c
curoli <curoli@gmail.com>
2021-03-04 21:10:08 +0100

    Advancing version from 1.7.6 to 1.7.7.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "1.7.6"
+  val version: String = "1.7.7"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cmd=$@
-image=lunaris-variant-mask-server:1.7.6
+image=lunaris-variant-mask-server:1.7.7
 
 echo "This is Lunaris Variant Mask Server Docker run script"
 
