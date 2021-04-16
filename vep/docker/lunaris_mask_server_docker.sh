ea04e75e8aed881988c04da91d7b7d67ba4add11
curoli <curoli@gmail.com>
2021-04-16 17:36:51 -0400

    Fix exons file location; advance version from 2.0.0-RC2 to 2.0.0-RC3.

--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-image=lunaris-variant-mask-server:2.0.0-RC2
+image=lunaris-variant-mask-server:2.0.0-RC3
 lunaris_vep_dir=$HOME/lunaris/vep
 inputs_dir=$lunaris_vep_dir/inputs
 results_dir=$lunaris_vep_dir/results
