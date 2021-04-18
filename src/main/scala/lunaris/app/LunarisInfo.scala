3191be7d766e7e654de768b5a82a2e51f048a4f0
curoli <curoli@gmail.com>
2021-04-18 17:45:16 -0400

    Diagnostics; change version from 2.0.0-DBG10 to 2.0.0-DBG11.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "2.0.0-DBG10"
+  val version: String = "2.0.0-DBG11"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
