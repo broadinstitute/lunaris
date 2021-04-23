8a6151a9769ecc68cd2d745335c886d05b298006
curoli <curoli@gmail.com>
2021-04-23 12:21:24 -0400

    Fix VcfSorterTest.

--- src/test/scala/lunaris/vep/VcfSorterTest.scala
+++ src/test/scala/lunaris/vep/VcfSorterTest.scala
@@ -7,25 +7,26 @@ import scala.sys.process._
 import java.nio.charset.StandardCharsets
 
 class VcfSorterTest extends AnyFunSuite {
+  private val t = "\t"
   private val vcfContentUnsorted =
-    """#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT
-      |1\t196857281\t1:196857281_A/ATGT\tA\tATGT
-      |3\t33166006\t3:33166006_G/GTC\tG\tGTC
-      |1\t193038186\t1:193038186_T/TGGA\tT\tTGGA
-      |2\t179412617\t2:179412617_T/TG\tT\tTG
-      |1\t200860688\t1:200860688_T/TA\tT\tTA
-      |2\t179330497\t2:179330497_C/CTA\tC\tCTA
-      |""".stripMargin
+    s"""#CHROM${t}POS${t}ID${t}REF${t}ALT${t}QUAL${t}FILTER${t}INFO${t}FORMAT
+       |1${t}196857281${t}1:196857281_A/ATGT${t}A${t}ATGT
+       |3${t}33166006${t}3:33166006_G/GTC${t}G${t}GTC
+       |1${t}193038186${t}1:193038186_T/TGGA${t}T${t}TGGA
+       |2${t}179412617${t}2:179412617_T/TG${t}T${t}TG
+       |1${t}200860688${t}1:200860688_T/TA${t}T${t}TA
+       |2${t}179330497${t}2:179330497_C/CTA${t}C${t}CTA
+       |""".stripMargin
 
   private val vcfContentSorted =
-    """#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT
-      |1\t193038186\t1:193038186_T/TGGA\tT\tTGGA
-      |1\t196857281\t1:196857281_A/ATGT\tA\tATGT
-      |1\t200860688\t1:200860688_T/TA\tT\tTA
-      |2\t179330497\t2:179330497_C/CTA\tC\tCTA
-      |2\t179412617\t2:179412617_T/TG\tT\tTG
-      |3\t33166006\t3:33166006_G/GTC\tG\tGTC
-      |""".stripMargin
+    s"""#CHROM${t}POS${t}ID${t}REF${t}ALT${t}QUAL${t}FILTER${t}INFO${t}FORMAT
+       |1${t}193038186${t}1:193038186_T/TGGA${t}T${t}TGGA
+       |1${t}196857281${t}1:196857281_A/ATGT${t}A${t}ATGT
+       |1${t}200860688${t}1:200860688_T/TA${t}T${t}TA
+       |2${t}179330497${t}2:179330497_C/CTA${t}C${t}CTA
+       |2${t}179412617${t}2:179412617_T/TG${t}T${t}TG
+       |3${t}33166006${t}3:33166006_G/GTC${t}G${t}GTC
+       |""".stripMargin
 
   def doTest(inputFileName: String, outputFileName: String): Unit = {
     val tempFolder = File.newTemporaryDirectory()
