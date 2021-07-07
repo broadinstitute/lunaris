6799d8dc52ef5ece6cf6c5f0654ab36ddeed0f30
curoli <curoli@gmail.com>
2021-07-07 19:05:43 -0400

    Remove removal of non-existing package sbt in Dockerfile; advancing version to 3.0.0-RC8.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.0.0-RC7"
+  val version: String = "3.0.0-RC8"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
