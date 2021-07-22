09f05d97f34fc139e896232186994556fe90a55f
curoli <curoli@gmail.com>
2021-07-21 21:43:35 -0400

    Using Selene 1.2.1 with fixed regions filtering; advancing version to 3.0.1-RC2.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.0.1-RC1"
+  val version: String = "3.0.1-RC2"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
