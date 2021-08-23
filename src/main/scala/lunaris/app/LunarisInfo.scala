ca0a76b790e225265787be3d20f241124e1add02
curoli <curoli@gmail.com>
2021-08-23 10:54:45 -0400

    Try newer Rust Docker image for build; advancing version to 3.3.0-RC3.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.3.0-RC2"
+  val version: String = "3.3.0-RC3"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
