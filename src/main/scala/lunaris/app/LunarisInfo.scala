1c4eb1a32e0b7d1bafe6055a1cb2f87a6e6534e5
curoli <curoli@gmail.com>
2021-08-24 16:08:17 -0400

    Upgrading Selene to 2.1.7 to strip NUL bytes; advancing version to 3.3.1-RC4.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.3.1-RC3"
+  val version: String = "3.3.1-RC4"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
