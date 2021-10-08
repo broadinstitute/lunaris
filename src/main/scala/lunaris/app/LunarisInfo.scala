0bc0a18cc5a92f971c8f318ed159b766985fb4b3
curoli <curoli@gmail.com>
2021-10-07 20:02:12 -0400

    Upgrading Selene to 2.2.7; advancing version to 3.5.2-RC6.

--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.5.2-RC5"
+  val version: String = "3.5.2-RC6"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
