58ea3877c46484dc6a33f5dae0709b117de6c457
curoli <curoli@gmail.com>
2021-10-21 18:46:50 -0400

    Fix Pos column in Docker-based configuration; advancing version to 3.5.2-RC8.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 import sbt.project
 
-val lunarisV = "3.5.2-RC7"
+val lunarisV = "3.5.2-RC8"
 val scalaV = "2.13.3"
 val yootilzV = "0.1.5"
 val scalaTestV = "3.2.0"
--- src/main/scala/lunaris/app/LunarisInfo.scala
+++ src/main/scala/lunaris/app/LunarisInfo.scala
@@ -1,6 +1,6 @@
 package lunaris.app
 
 object LunarisInfo {
-  val version: String = "3.5.2-RC7"
+  val version: String = "3.5.2-RC8"
   val versionLong: String = s"Lunaris Version $version (c) 2020-21 Broad Institute"
 }
