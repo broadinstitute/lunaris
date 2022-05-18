253c9bfcb3e001fc6f398322ca76c8d781e808bd
curoli <curoli@gmail.com>
2022-05-18 15:05:35 -0400

    Advancing Selene from 2.3.0; advancing version to 3.7.2;

--- vep/docker/lunaris_mask_server_docker.sh
+++ vep/docker/lunaris_mask_server_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-image=lunaris-variant-mask-server:3.7.1
+image=lunaris-variant-mask-server:3.7.2
 lunaris_vep_dir=$HOME/lunaris/vep
 inputs_dir=$lunaris_vep_dir/inputs
 results_dir=$lunaris_vep_dir/results
